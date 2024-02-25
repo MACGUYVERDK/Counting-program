f211dbb5ae7bde29d8d3973f4245d073766142b7
MACGUYVERDK <macguyverdk@hotmail.com>
2024-02-25 19:45:39 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿
+// Program for counting number of digits in example number
+
+
+
+int number = 6529985;
+string numberString = number.ToString();
+char[] letters = numberString.ToCharArray();
+Console.WriteLine("Example number " + number);
+
+List<int> counters = new List<int>(10);
+
+
+for (int i = 0; i < 10; i++)
+{
+    counters.Add(0);
+}
+
+foreach (char letter in letters)
+{
+    if(char.IsDigit(letter))
+    {
+        int digit = letter - '0';
+        counters[digit]++;
+    }
+}
+
+for (int i = 0;i < 10; i++ )
+{
+    Console.WriteLine(i + " = " + counters[i]);
+}
